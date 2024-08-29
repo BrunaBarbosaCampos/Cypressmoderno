b1cb8f051e3d410634d9da35990a7ec10197b3e0
Bruna Natália Barbosa Campos <brunanataliacampos12602@gmail.com>
2024-08-28 23:16:22 -0300

    commit

--- cypress/e2e/Test_API/Backend.cy.js
+++ cypress/e2e/Test_API/Backend.cy.js
@@ -5,33 +5,6 @@ describe('Works with a project', () => {
 		// cy.visit('https://barrigareact.wcaquino.me')
 	})
 
-	// it('Insert account', () => {
-	// 	cy.request({
-	// 		method: 'POST',
-	// 		url: 'https://barrigarest.wcaquino.me/signin',
-	// 		body: {
-	// 			email: 'bruna@teste',
-	// 			redirecionar: false,
-	// 			senha: 'b@t'
-	// 		}
-	// 	})
-	// 		.its('body.token')
-	// 		.should('not.be.empty')
-	// 		.then((token) => {
-	// 			cy.request({
-	// 				url: 'https://barrigarest.wcaquino.me/contas',
-	// 				method: 'POST',
-	// 				headers: {
-	// 					Authorization: `bearer ${token}`,
-	// 					'Content-Type': 'application/json'
-	// 				},
-	// 				body: {
-	// 					nome: 'Conta criada por API'
-	// 				}
-	// 			}).then((res) => console.log(res))
-	// 		})
-	// })
-
 	it('Insert account', () => {
 		cy.request({
 			method: 'POST',
@@ -45,11 +18,10 @@ describe('Works with a project', () => {
 			.its('body.token')
 			.should('not.be.empty')
 			.then((token) => {
-				console.log('Token:', token)
 				cy.request({
 					url: 'https://barrigarest.wcaquino.me/contas',
 					method: 'POST',
-					headers: { Authorization: `Bearer ${token}` },
+					headers: { Authorization: `JWT ${token}` },
 					body: {
 						nome: 'Conta criada por API'
 					}
